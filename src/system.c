0fe4375b0681ec7d40dc664e8551f59ff436e036
Tyler Hicks <tyhicks@canonical.com>
2017-08-11 15:46:49 +0000

    system: runtime check if a filter flag is supported by the kernel
    As new filter flags are added to the kernel, libseccomp needs a way to
    verify that a filter flag is not only valid but also supported by the
    current kernel at runtime. A good way of doing that is by attempting to
    enter filter mode, with the flag bit(s) in question set, and a NULL
    pointer for the args parameter of seccomp(2). EFAULT indicates that the
    flag is valid and EINVAL indicates that the flag is invalid. This patch
    errs on the side of caution and treats any errno, besides EFAULT, as
    indicating that the flag is invalid.
    
    This check should be safe to use for the existing
    SECCOMP_FILTER_FLAG_TSYNC flag so this patch enables the check for that
    flag.
    
    Signed-off-by: Tyler Hicks <tyhicks@canonical.com>

--- src/system.c
+++ src/system.c
@@ -101,6 +101,34 @@ supported:
  * Check to see if a seccomp() flag is supported
  * @param flag the seccomp() flag
  *
+ * This function attempts to use seccomp() to check if the specified seccomp()
+ * flag is supported. If the flag is supported one is returned, zero
+ * otherwise.
+ */
+static int _sys_chk_seccomp_flag(int flag)
+{
+	int rc;
+
+	if (!sys_chk_seccomp_syscall())
+		goto unsupported;
+
+	/* this is an invalid call because the last argument is NULL, but
+	 * depending on the errno value of EFAULT we can guess if the
+	 * filter flag is supported or not */
+	rc = syscall(_nr_seccomp, SECCOMP_SET_MODE_FILTER, flag, NULL);
+	if (rc < 0 && errno == EFAULT)
+		goto supported;
+
+unsupported:
+	return 0;
+supported:
+	return 1;
+}
+
+/**
+ * Check to see if a seccomp() flag is supported
+ * @param flag the seccomp() flag
+ *
  * This function checks to see if a seccomp() flag is supported by the system.
  * If the flag is supported one is returned, zero if unsupported, negative
  * values on error.
@@ -110,7 +138,7 @@ int sys_chk_seccomp_flag(int flag)
 {
 	switch (flag) {
 	case SECCOMP_FILTER_FLAG_TSYNC:
-		return sys_chk_seccomp_syscall();
+		return _sys_chk_seccomp_flag(flag);
 	}
 
 	return -EOPNOTSUPP;
